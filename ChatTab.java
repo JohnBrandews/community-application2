80f71c63929817a2169570cf4cc67a47a9aad05c
JohnBrandews <johnbrandews@gmail.com>
2024-03-15 21:38:53 +0300

    added chattab

---
+++ ChatTab.java
@@ -0,0 +1,17 @@
+
+// ChatTab.java
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class ChatTab extends VBox {
+
+    public ChatTab() {
+        // Create and add components for the Chat tab
+        Label chatLabel = new Label("Community Chat");
+        getChildren().add(chatLabel);
+
+        // Add additional components and functionality as needed
+        // (e.g., messaging interface, group creation, notification system, etc.)
+    }
+}
+
