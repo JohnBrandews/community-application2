98364cd3bc8d553fcbc346c515e556829f3cef45
JohnBrandews <johnbrandews@gmail.com>
2024-03-15 21:40:09 +0300

    added some files

---
+++ com.example/com/example/com/example/com/example/com/example/HelloJavaFX.java
@@ -0,0 +1,25 @@
+ 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloJavaFX extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 300, 200);
+
+        primaryStage.setTitle("Hello JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
